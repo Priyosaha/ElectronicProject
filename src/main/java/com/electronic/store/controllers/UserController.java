8f029790268ed229384cae1818b4b68392a3d69f
Priyosaha <priyosaha339@gmail.com>
2025-01-25 14:27:19 +0530

    fixing issues with lombok

--- src/main/java/com/electronic/store/controllers/UserController.java
+++ src/main/java/com/electronic/store/controllers/UserController.java
@@ -6,9 +6,9 @@ import com.electronic.store.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -17,10 +17,37 @@ public class UserController {
     @Autowired
     UserService userService;
 
-
+    @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
 
         UserDto user = userService.createUser(userDto);
         return  new ResponseEntity<>(user, HttpStatus.CREATED);
     }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable("userId") String userId,
+                                              @RequestBody UserDto userDto) {
+
+        UserDto user = userService.updatedUser(userDto,userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+
+    }
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>("USER IS DELETED", HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> list = userService.getAllUser();
+        return new ResponseEntity<>(list,HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable("userId") String userId) {
+        UserDto user = userService.getUserById(userId);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
 }
